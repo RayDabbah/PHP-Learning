b5c8191d19e672209e6202bd6e8868842ad29509
raydabbah <dabbahray@yahoo.com>
2017-08-04 12:15:36 -0400

    moved it to a new directory

---
+++ practice/index.php
@@ -0,0 +1,40 @@
+<?php
+require 'functions.php';
+$greet = 'Yankel Shmerelstein';
+$greeting = "Hello $greet!!!!";
+
+$foods = [
+    'Kibbe',
+    'Cholent',
+    'Lahmaagine',
+    'Farfel'
+];
+$animals = [
+    'Cats',
+    'Dogs',
+    'Turtles',
+    'Platypuses',
+    'Otters',
+    'Rabbits'
+];
+$animals[] = 'Aligators';
+$ages = [
+    'Yankel' => 45,
+    'Shmerel' => 62,
+    'David' => 12,
+    'Pinchos' => 21
+];
+$ages['Gedaliah'] = 76;
+// var_dump($animals);
+// var_dump($ages);
+// echo 'Good morning! ' . $greet . ' !!!';
+$tasks = [
+    'toDo' => 'vacuum',
+    'assignedTo' => 'נחום',
+    'due by' => 'Thursday',
+    'isDone' => true,
+    'enjoyable' => true
+];
+ageCheck(2);
+
+require 'views.php';
