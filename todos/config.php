b6d573ac4de62f704bdfdc9245ea417985bd6acc
raydabbah <dabbahray@yahoo.com>
2017-08-08 01:31:17 -0400

    now it works with quotes for all! no clue why!

--- todos/config.php
+++ todos/config.php
@@ -1,10 +1,10 @@
 <?php
 return [
     'db' => [
-        host => 'mysql:host=localhost;',
-        dbName => 'dbname=myPHP',
-        name => 'rd',
-        password => '123',
-        options => []
+        'host' => 'mysql:host=localhost;',
+        'dbName' => 'dbname=myPHP',
+        'name' => 'rd',
+        'password' => '123',
+        'options' => []
     ]
 ];
