f2e1a63ec8500c3a5f6a6defac90bab89bbf14f5
raydabbah <dabbahray@yahoo.com>
2017-08-08 00:18:44 -0400

    messy item

--- todos/database/Connector.php
+++ todos/database/Connector.php
@@ -1,10 +1,14 @@
 <?php
+require 'config.php';
+$host; $dbName; $name; $password;
+
 class Connector
 {
     public static function connect()
     {
         try {
-            return $pdo = new PDO('mysql:host=localhost;dbname=myPHP', 'rd', '123');
+            // return $pdo = new PDO('mysql:host=localhost;dbname=myPHP', 'rd', '123');
+            return $pdo = new PDO($host . $dbName, $name, $password);
         } catch (PDOException $e) {
             die($e->getMessage());
         }
