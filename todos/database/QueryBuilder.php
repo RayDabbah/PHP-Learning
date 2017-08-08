69f77140bc3f996d455838a7c9672b1373a0fcc6
raydabbah <dabbahray@yahoo.com>
2017-08-07 23:01:15 -0400

    refactored query into new Query class

---
+++ todos/database/QueryBuilder.php
@@ -0,0 +1,15 @@
+<?php
+class Query
+{
+    protected $pdo;
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+    public function selectAll($table, $class)
+    {
+        $statement = $this->pdo->prepare("SELECT * FROM $table");
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_CLASS, $class);
+    }
+}
--- todos/index.php
+++ todos/index.php
@@ -1,12 +1,11 @@
 <?php
-// require 'database/QueryBuilder.php';
+require 'database/QueryBuilder.php';
 require 'database/Connecter.php';
-require 'functions.php';
 require 'Task.php';
 require 'User.php';
-/* $pdo = */ Connecter::connect();
-// $tasks = new Query($pdo, 'todos', 'Task');
-// $users = new Query($pdo, 'users', 'User');
-$tasks = fetchAllData('todos', 'Task');
-$users = fetchAllData('users', 'User');
+ $pdo =  Connecter::connect();
+$taskQuery = new Query($pdo);
+ $tasks = $taskQuery->selectAll('todos', 'Task');
+$userQuery = new Query($pdo);
+$users = $userQuery->selectAll('users', 'User');
 require 'index.view.php';
