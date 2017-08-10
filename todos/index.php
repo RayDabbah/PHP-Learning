e58a16885ea56263346fc732770df59196d1cd02
raydabbah <dabbahray@yahoo.com>
2017-08-09 22:21:06 -0400

    added a static method to Router.php to  'clean up' more

--- todos/index.php
+++ todos/index.php
@@ -1,9 +1,9 @@
 <?php
 $Query = require 'core/bootstrap.php';
 
-$router = new Router;
-require 'routes.php';
+// $router = new Router;
 // die(var_dump($_SERVER));
-$uri = trim($_SERVER['REQUEST_URI'],'/');
-require $router->direct($uri);
+$uri = trim($_SERVER['REQUEST_URI'], '/');
+require Router::get('routes.php')
+->direct($uri);
 
