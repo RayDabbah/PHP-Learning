790d6794aee152f0de3cd2c8f8a2f5fc9affaea4
raydabbah <dabbahray@yahoo.com>
2017-08-14 13:37:56 -0400

    added a name form not yet working

---
+++ todos/views/nameForm.php
@@ -0,0 +1,8 @@
+<?php include 'partials/header.php'?>
+<h1>Enter your name.</h1>
+<form action="/name"  method="POST">
+<input type="text" name="name">
+<input type="submit">
+</form>
+
+<?php include 'partials/footer.php'?>
