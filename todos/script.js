0ca24c084268d73b01bbdfc3433cc26e6a4706c4
raydabbah <dabbahray@yahoo.com>
2017-08-15 12:58:07 -0400

    changed li cursor to pointer

--- todos/script.js
+++ todos/script.js
@@ -1,7 +1,8 @@
 var todoSpan = document.querySelectorAll('#todoList span');
 var todoLi = (document.querySelectorAll('#todoList li'));
-for(let i = 0; i<= todoSpan.length; i++){
+for (let i = 0; i <= todoSpan.length; i++) {
     todoLi[i].addEventListener('click', function (e) {
+        e.stopPropagation();
         todoSpan[i].classList.toggle('crossedout');
         todoLi[i].classList.toggle('linethrough');
         todoLi[i].classList.toggle('incompleteTodo');
