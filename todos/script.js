ecad4954997b6dc707961df183667bda8c0ba4d2
raydabbah <dabbahray@yahoo.com>
2017-08-14 22:43:19 -0400

    deleted console.log and comments out of script.js

--- todos/script.js
+++ todos/script.js
@@ -1,22 +1,6 @@
-// Array.from(document.querySelectorAll('#todoList>li')).forEach(function (li) {
-//     li.addEventListener('click', function (e) {
-//         li.classList.toggle('linethrough');
-//         li.classList.toggle('incompleteTodo');
-//         console.log(e.target), false;
-//     })
-// });
-
-// Array.from(document.querySelectorAll('#todoList span')).forEach(function (li) {
-//     li.addEventListener('click', function (e) {
-//         li.classList.toggle('crossedout');
-//         console.log(e.target), false;
-//     })
-// });
-// document.querySelectorAll('#todoList span'));
 var todoSpan = document.querySelectorAll('#todoList span');
 var todoLi = document.querySelectorAll('#todoList li');
 for(let i = 0; i<= todoSpan.length; i++){
-    console.log(todoSpan[i]);
     todoLi[i].addEventListener('click', function (e) {
         todoSpan[i].classList.toggle('crossedout');
         todoLi[i].classList.toggle('linethrough')
