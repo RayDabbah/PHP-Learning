62161f7c3808f828ae7546d0c3e51c94c92ba6a7
raydabbah <dabbahray@yahoo.com>
2017-08-17 22:31:20 -0400

    added composer

--- todos/core/bootstrap.php
+++ todos/core/bootstrap.php
@@ -1,10 +1,5 @@
 <?php
 $app =[];
-require 'core/Router.php';
-require 'core/database/QueryBuilder.php';
-require 'core/database/Connector.php';
 $app['config'] = require 'config.php';
-require 'core/Task.php';
-require 'core/User.php';
 $app['database'] = new Query(
     Connector::connect($app['config']['db']));
