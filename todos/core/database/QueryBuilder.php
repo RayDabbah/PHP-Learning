29d3af232fe5d8f632f8da1a35e0c8787e7a535a
raydabbah <dabbahray@yahoo.com>
2017-08-21 22:39:31 -0400

    added bindParam (I think to protect from SQL injedtion) to all possible db queries

--- todos/core/database/QueryBuilder.php
+++ todos/core/database/QueryBuilder.php
@@ -14,22 +14,31 @@ class Query
     }
     public function delete($table, $id)
     {
-        $statement = $this->pdo->prepare("DELETE FROM $table WHERE id=$id");
+        $statement = $this->pdo->prepare("DELETE FROM $table WHERE id=:id");
+        $statement->bindParam(':id', $id);
         $statement->execute();
     }
     public function addUser($table, $username, $email, $password)
     {
-        $statement = $this->pdo->prepare("INSERT INTO $table (`username`, `email`, `password`) VALUES ('$username', '$email', '$password');");
+        $statement = $this->pdo->prepare("INSERT INTO $table (`username`, `email`, `password`) VALUES (:username, :email, :password);");
+        $statement->bindParam(':username', $username);
+        $statement->bindParam(':email', $email);
+        $statement->bindParam(':password', $password);
         $statement->execute();
     }
     public function addTask($table, $description, $completed)
     {
-        $statement = $this->pdo->prepare("INSERT INTO $table (`description`, `completed`) VALUES ('$description', '$completed');");
+        $statement = $this->pdo->prepare("INSERT INTO $table (`description`, `completed`) VALUES (:description, :completed);");
+        $statement->bindParam(':description', $description);
+        $statement->bindParam(':completed', $completed);
         $statement->execute();
     }
     public function updateTask($table, $description, $completed, $id)
     {
-        $statement = $this->pdo->prepare("UPDATE $table SET description='$description', completed='$completed' WHERE id=$id;");
+        $statement = $this->pdo->prepare("UPDATE $table SET description=:description, completed=:completed WHERE id=:id;");
+        $statement->bindParam(':description', $description);
+        $statement->bindParam(':completed', $completed);
+        $statement->bindParam(':id', $id);
         $statement->execute();
     }
 }
