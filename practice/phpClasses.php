20e092b71bc95fc414443a6fab33c9193ad81c39
raydabbah <dabbahray@yahoo.com>
2017-10-03 01:10:33 -0400

    add learning PHP classes for OOP

---
+++ practice/phpClasses.php
@@ -0,0 +1,47 @@
+<?php
+class Learn
+{
+
+    public $property1 = 'I am the 1st property variable';
+
+    public function __construct()
+    {
+        echo __CLASS__ . ' was called!';
+    }
+
+    public function __destruct()
+    {
+        echo __CLASS__ . ' was destroyed!';
+    }
+
+    public function setProperty($property1)
+    {
+        $this->property1 = $property1;
+    }
+    public function getProperty()
+    {
+        return $this->property1;
+    }
+    public function __toString(){
+        echo 'Testing the "magic" __toString method.' . "\n\r";
+        return $this->getProperty();
+    }
+}
+$learning = new Learn;
+$learning2 = new Learn;
+
+echo "\n\r";
+// unset($learning);
+echo $learning->getProperty();
+echo "\n\r";
+ $learning->setProperty('This is the new property');
+echo "\n\r";
+echo $learning->getProperty();
+echo $learning2->getProperty();
+echo "\n\r";
+ $learning2->setProperty('This is the 2nd new property');
+echo "\n\r";
+echo $learning2->getProperty();
+echo "\n\r";
+echo $learning;
+?>
