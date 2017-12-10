0614c8552b9f878f221413c8e1db7b058b474c66
raydabbah <dabbahray@yahoo.com>
2017-12-09 22:36:20 -0500

    abstract classes and methods

--- practice/oop.php
+++ practice/oop.php
@@ -1,15 +1,34 @@
 <?php
-class Person
+abstract class Creature
+{
+    protected $age;
+    protected $feet;
+
+    public function __construct($feet = 4)
+    {
+
+        $this->feet = $feet;
+        
+    }
+    public function getFeet()
+    {
+        return $this->feet;
+    }
+    abstract public function getAge();
+}
+
+
+class Person extends Creature
 {
 
     public $name;
 
-    protected $age;
 
-    public function __construct($name)
-    {
-        $this->name = $name;
-    }
+    // public function __construct($name, $feet)
+    // {
+    //     $this->name = $name;
+    //     $this->feet = $feet;
+    // }
     public function setAge($age)
     {
         if ($age < 21) {
@@ -17,14 +36,27 @@ class Person
         }
         $this->age = $age;
     }
-    public function getAge($age)
+    public function getAge()
     {
         return $this->age;
     }
-
+    
+}
+class Cat extends Creature
+{
+    public function getAge(){
+        return $this->age;
+    }
 }
 
-$person = new Person('Yankel');
-$person->setAge(52);
-echo $person->age;
+$person = new Person();
+$person->setAge(105);
+// $person->setFeet(2);
+echo $person->getFeet() . "\n";
+echo $person->getAge() . "\n";
+
 var_dump($person);
+
+echo (new Cat())->getFeet() . "\n";
+
+
